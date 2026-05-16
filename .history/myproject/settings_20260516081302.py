50f7f83075d5f63d777eb8919cd2465b6575d336
babukrishnan <90099438+babukrishnan@users.noreply.github.com>
2026-05-16 08:13:56 +0530

    Fix CSRF trusted origins

--- .history/myproject/settings_20260516081302.py
+++ .history/myproject/settings_20260516081302.py
@@ -167,7 +167,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
