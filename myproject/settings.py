39ab7b2733a2eadb77a764c74f82b0a280eb83ff
Babu <90099438+babukrishnan@users.noreply.github.com>
2026-05-22 20:03:40 +0530

    DB fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -109,19 +109,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'farmerdb',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-    #  'default': dj_database_url.config(
-    #     default=os.environ.get("DATABASE_URL"),
-    #     conn_max_age=600,
-    #     ssl_require=True
-    # )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'farmerdb',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '123456',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+     'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
